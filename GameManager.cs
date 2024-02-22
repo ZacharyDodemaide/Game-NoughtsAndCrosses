714a8fe883e9f424a7d3f31f050e0534eec64981
Zac D <zacharydodemaide@outlook.com>
2024-02-22 22:47:42 +1100

    first commit

---
+++ GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class GameManager
+    {
+    public void play(WildState wildState)
+    {
+        string input;
+        do
+        {
+            Console.WriteLine("Enter (row, column, token) or Q to quit:");
+            input = Console.ReadLine();
+            Console.WriteLine();
+
+            string token;
+            string[] values = input.Split(',');
+            int.TryParse(values[0], out int row);
+            int.TryParse(values[1], out int col);
+            token = values[2];
+
+            if (token == "X" || token == "O")
+            {
+                wildState.Board.UpdateBoard(row, col, token);
+                wildState.Board.ShowBoard();
+
+            }
+            else if (input != "Q")
+            {
+                Console.WriteLine("Invalid input. Please enter X, O, or Q.");
+            }
+        } while (input != "Q");
+    }
+    
+    }
+
---
+++ GameState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    public interface IGameState
+    {
+        IBoard Board { get; set; }
+        int NextPlayer { get; set; }
+    }
+
+    public class WildState : IGameState
+    {
+        public IBoard Board { get; set; }
+        public int NextPlayer { get; set; }
+
+        public WildState(WildBoard wb)
+        {
+            Board = wb;
+            NextPlayer = 1; // Let's say 1 represents the first player
+        }
+    }
